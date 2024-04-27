75407e95514687c5613ced8e0d6c977192e49273
Jeril Kuriakose <jeril.work@gmail.com>
2024-04-27 14:50:28 +0300

    Update model_adapter.py

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -2409,7 +2409,7 @@ class JaisAdapter(BaseModelAdapter):
         return any(substring in model_path.lower() for substring in model_names)
 
     def get_default_conv_template(self, model_path: str) -> Conversation:
-        return get_conv_template("jais_default_en")
+        return get_conv_template("jais_default_ar")
 
 
 class AllamAdapter(BaseModelAdapter):
