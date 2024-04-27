302ee368199ffb8c37e99f01ee6b0780cd30a1e2
Jeril Kuriakose <jeril.work@gmail.com>
2024-04-27 12:21:56 +0300

    Update common.py

--- fastchat/llm_judge/common.py
+++ fastchat/llm_judge/common.py
@@ -408,30 +408,50 @@ def chat_completion_openai(model, conv, temperature, max_tokens, api_dict=None):
     if api_dict is not None:
         openai.api_base = api_dict["api_base"]
         openai.api_key = api_dict["api_key"]
+        
+    use_local_azure = os.getenv("USE_LOCAL_AZURE", None)
+    if use_local_azure:
+        openai.api_type = os.getenv("LOCAL_API_TYPE")
+        openai.api_base = os.getenv("LOCAL_API_BASE")
+        openai.api_version = os.getenv("LOCAL_API_VERSION")
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        output = API_ERROR_OUTPUT
+        for _ in range(API_MAX_RETRY):
+            try:
+                messages = conv.to_openai_api_messages()
+                response = openai.ChatCompletion.create(
+                    engine=os.getenv("LOCAL_ENGINE"),
+                    messages=messages,
+                    n=1,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                )
+                output = response["choices"][0]["message"]["content"]
+                break
+            except openai.error.OpenAIError as e:
+                print(type(e), e)
+                time.sleep(API_RETRY_SLEEP)
 
-    openai.api_type = os.getenv("LOCAL_API_TYPE")
-    openai.api_base = os.getenv("LOCAL_API_BASE")
-    openai.api_version = os.getenv("LOCAL_API_VERSION")
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
-    output = API_ERROR_OUTPUT
-    for _ in range(API_MAX_RETRY):
-        try:
-            messages = conv.to_openai_api_messages()
-            response = openai.ChatCompletion.create(
-                engine=os.getenv("LOCAL_ENGINE"),
-                messages=messages,
-                n=1,
-                temperature=temperature,
-                max_tokens=max_tokens,
-            )
-            output = response["choices"][0]["message"]["content"]
-            break
-        except openai.error.OpenAIError as e:
-            print(type(e), e)
-            time.sleep(API_RETRY_SLEEP)
-
-    return output
+        return output
+    else:
+        output = API_ERROR_OUTPUT
+        for _ in range(API_MAX_RETRY):
+            try:
+                messages = conv.to_openai_api_messages()
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                    n=1,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                )
+                output = response["choices"][0]["message"]["content"]
+                break
+            except openai.error.OpenAIError as e:
+                print(type(e), e)
+                time.sleep(API_RETRY_SLEEP)
+    
+        return output
 
 
 def chat_completion_openai_azure(model, conv, temperature, max_tokens, api_dict=None):
