0cb8b4291a496d57097ca78cb227588bbb2e9b2c
Jeril Kuriakose <jnkuriakose@ncai2.onmicrosoft.com>
2024-05-08 16:43:40 +0000

    patch fix for v1.12.0.3

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -2455,7 +2455,14 @@ class AllamAdapterV1_12(BaseModelAdapter):
     """Model adapter for ALLaM model for ALLAM benchmark"""
 
     def match(self, model_path: str):
-        model_names = ["allam-13b-v1-12", "allam_13b_v1_12", "allam-13b-v1-11-2", "allam_13b_v1_11_2"]
+        model_names = [
+            "allam-13b-v1-12", 
+            "allam_13b_v1_12", 
+            "allam-13b-v1-11-2", 
+            "allam_13b_v1_11_2",
+            "allam-13b-v1-12-0-3",
+            "allam_13b_v1_12_0_3"
+        ]
         return any(substring in model_path.lower() for substring in model_names)
 
     def get_default_conv_template(self, model_path: str) -> Conversation:
