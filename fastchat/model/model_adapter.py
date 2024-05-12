7e21a9dc96007562a5dc1e10cbaa920685535c01
Jeril Kuriakose <jnkuriakose@ncai2.onmicrosoft.com>
2024-05-12 15:39:11 +0000

    v1.11.0.8 change

--- fastchat/model/model_adapter.py
+++ fastchat/model/model_adapter.py
@@ -2496,7 +2496,7 @@ class AllamAdapterV1_12_2(BaseModelAdapter):
 
     def get_default_conv_template(self, model_path: str) -> Conversation:
         return get_conv_template("allam_zero_shot_in_house_without_sys_v1_12_2")
-    
+
 
 class AllamAdapterV1_12(BaseModelAdapter):
     """Model adapter for ALLaM model for ALLAM benchmark"""
@@ -2505,6 +2505,8 @@ class AllamAdapterV1_12(BaseModelAdapter):
         model_names = [
             "allam-13b-v1-12",
             "allam_13b_v1_12",
+            "allam-13b-v1-11-0",
+            "allam_13b_v1_11_0",
             "allam-13b-v1-11-2",
             "allam_13b_v1_11_2",
             "allam-13b-v1-12-0-3",
