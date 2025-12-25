37fd66e5cc58d67f3809dc2854c71cff94a7fb04
Jeril Kuriakose <jeril.work@gmail.com>
2025-12-25 16:12:05 +0000

    bug fix

--- fastchat/serve/gradio_block_arena_anony.py
+++ fastchat/serve/gradio_block_arena_anony.py
@@ -576,7 +576,7 @@ def build_side_by_side_ui_anony(models):
         regenerate_btn = gr.Button(value="🔄  Regenerate", interactive=False)
         share_btn = gr.Button(value="📷  Share")
 
-    with gr.Accordion("Parameters", open=False, visible=False) as parameter_row:
+    with gr.Accordion("Parameters", open=False) as parameter_row:
         temperature = gr.Slider(
             minimum=0.0,
             maximum=1.0,
